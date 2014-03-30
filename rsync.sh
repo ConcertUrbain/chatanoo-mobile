9d00146be0827e06679827ccade2cf2cc2c1dbc2
Mathieu Desvé <mathieu.desve@me.com>
2014-03-30 22:45:33 +0100

    Fix git and rsync

--- rsync.sh
+++ rsync.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rsync -avz --delete -e ssh . "root@ns368978.ovh.net:/var/www/vhosts/chatanoo.org/projects/mjc-94/m/prod/" --exclude-from 'rsync.exclude'
+rsync -avz --delete -e ssh . "root@ns368978.ovh.net:/var/www/vhosts/chatanoo.org/projects/handicap/m/prod/" --exclude-from 'rsync.exclude'
